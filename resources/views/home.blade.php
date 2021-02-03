fa6fc7f5cc0d125b65fcf5e036b37f1419533629
Unknown <wafaek19@gmail.com>
2021-02-04 00:24:33 +0100

    add pictures

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -113,9 +113,9 @@
                   <img class="first-slide" src="{{ asset('images/offre6.jpg') }}" alt="First slide">
                   <div class="container">
                      <div class="carousel-caption relative">
-                        <h1>A propos de nous !</h1>
-                        <p>Achetez vos produits de parapharmacie au Maroc sur vitrine.ma. Notre société vous offre le plus grand choix de produits de parapharmacie au Maroc et aux meilleurs prix du marché. Vous pouvez commander vos produits en ligne sur notre site. Pour le paiement, vous avez le choix entre le paiement en espèce à la livraison ou le paiement électronique en utilisant votre carte bancaire. Les livraisons des produits de parapharmacie sur Casablanca, Rabat, Salé, Témara et Kénitra sont opérées par nos propres livreurs. </p>
-                        <a  href="#">Read More</a>
+                        <h1 style="color:black">A propos de nous !</h1>
+                        <p style="color:black">Achetez vos produits de parapharmacie au Maroc sur vitrine.ma. Notre société vous offre le plus grand choix de produits de parapharmacie au Maroc et aux meilleurs prix du marché. Vous pouvez commander vos produits en ligne sur notre site. Pour le paiement, vous avez le choix entre le paiement en espèce à la livraison ou le paiement électronique en utilisant votre carte bancaire. Les livraisons des produits de parapharmacie sur Casablanca, Rabat, Salé, Témara et Kénitra sont opérées par nos propres livreurs. </p>
+                        <a style="color:black" href="#">Read More</a>
                      </div>
                   </div>
                </div>
@@ -338,7 +338,7 @@
             <div class="row">
                <div class="col-md-12">
                   <div class="titlepage">
-                     <h2>Download  And  Use  For  Free  </h2>
+                     <h2>RECENT Products</h2>
                      <span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of <br>using Lorem Ipsum is that it has a more-or-less normal distribution of letters,</span>
                   </div>
                </div>
@@ -347,95 +347,20 @@
                <div class="col-md-12">
                   <div id="main_slider" class="carousel slide banner-main" data-ride="carousel">
                      <div class="carousel-inner">
-                        <div class="carousel-item active"> <img class="first-slide" src="images/banner.png" alt="First slide"> </div>
-                        <div class="carousel-item"> <img class="second-slide" src="images/banner.png" alt="Second slide"> </div>
-                        <div class="carousel-item"> <img class="third-slide" src="images/banner.png" alt="Third slide"> </div>
+                        <div class="carousel-item active"> <img class="first-slide" src="images/offre14.jpg" alt="First slide"> </div>
+                        <div class="carousel-item"> <img class="second-slide" src="images/offre13.png" alt="Second slide"> </div>
+                        <div class="carousel-item"> <img class="third-slide" src="images/offre16.jpg" alt="Third slide"> </div>
                      </div>
                      <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev"> <i class='fa fa-angle-left'></i></a> <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next"> <i class='fa fa-angle-right'></i></a> 
                   </div>
-                  <div class="read-more">
-                     <a href="#">download now</a>
-                  </div>
+              
                </div>
             </div>
          </div>
       </div>
       <!-- end Download -->
       <!-- Testimonial -->
-      <div id="testimonial" class="testimonial">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12">
-                  <div class="titlepage">
-                     <h3>Testimonial</h3>
-                  </div>
-               </div>
-            </div>
-            <div class="row">
-               <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                  <div id="testimonial_slider" class="carousel slide banner-bg" data-ride="carousel">
-                     <div class="carousel-inner">
-                        <div class="carousel-item active">
-                           <img class="first-slide" src="images/testimonial-img.png">
-                           <div class="container">
-                              <div class="carousel-caption relat">
-                                 <h3>Luka due</h3>
-                                 <span><i class="fa fa-quote-left"></i> ( foundery )<i class="fa fa-quote-right"></i></span>
-                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem  </p>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="carousel-item">
-                           <img class="second-slide" src="images/testimonial-img.png">
-                           <div class="container">
-                              <div class="carousel-caption relat">
-                                 <h3>Luka due</h3>
-                                 <span><i class="fa fa-quote-left"></i> ( foundery )<i class="fa fa-quote-right"></i></span>
-                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem  </p>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="carousel-item">
-                           <img class="third-slide" src="images/testimonial-img.png">
-                           <div class="container">
-                              <div class="carousel-caption relat">
-                                 <h3>Luka due</h3>
-                                 <span><i class="fa fa-quote-left"></i> ( foundery )<i class="fa fa-quote-right"></i></span>
-                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem  </p>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <a class="carousel-control-prev" href="#testimonial_slider" role="button" data-slide="prev"> <i class='fa fa-angle-right'></i></a> <a class="carousel-control-next" href="#testimonial_slider" role="button" data-slide="next"> <i class='fa fa-angle-left'></i></a> 
-                  </div>
-               </div>
-               <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                  <div class="contact">
-                     <h3>Contact Us</h3>
-                     <form>
-                        <div class="row">
-                           <div class="col-sm-12">
-                              <input class="contactus" placeholder="Name" type="text" name="Name">
-                           </div>
-                           <div class="col-sm-12">
-                              <input class="contactus" placeholder="Phone" type="text" name="Email">
-                           </div>
-                           <div class="col-sm-12">
-                              <input class="contactus" placeholder="Email" type="text" name="Email">
-                           </div>
-                           <div class="col-sm-12">
-                              <textarea class="textarea" placeholder="Message" type="text" name="Message"></textarea>
-                           </div>
-                           <div class="col-sm-12">
-                              <button class="send">Send</button>
-                           </div>
-                        </div>
-                     </form>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
+      
       <!-- end Testimonial --> 
       <!--  footer --> 
       <footr>
@@ -480,9 +405,7 @@
                   </div>
                </div>
             </div>
-            <div class="copyright">
-               <p>Copyright 2019 All Right Reserved By <a href="https://html.design/">Free html Templates</a></p>
-            </div>
+    
          </div>
       </footr>
       <!-- end footer -->
